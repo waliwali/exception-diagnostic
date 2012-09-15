fd9009a79cd2de268633bd03e5b20de6367203c7
bsivko <bsivko@3f74e1f5-aec5-da47-6e7e-2e542e26d3da>
2012-09-15 19:47:12 +0000

    inital import

---
+++ trunk/dev/samples/hello_world/prj.rb
@@ -0,0 +1,12 @@
+require 'mxx_ru/cpp'
+
+MxxRu::Cpp::exe_target {
+
+	required_prj "exception_diagnostic/prj.rb" 
+
+	implib_path 'lib'
+
+	target( "sample.hello_world" )
+
+	cpp_source( "main.cpp" )
+}
