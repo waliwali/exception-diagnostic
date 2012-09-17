00a7586e695b39423a0b23903bf14ae2acd59ebc
bsivko <bsivko@3f74e1f5-aec5-da47-6e7e-2e542e26d3da>
2012-09-17 14:05:12 +0000

    +class fields sample

--- trunk/dev/build.rb
+++ trunk/dev/build.rb
@@ -16,4 +16,5 @@ MxxRu::Cpp::composite_target( MxxRu::BUILD_ROOT ) {
 		required_prj "samples/hello_world/prj.rb" 
 		required_prj "samples/several/prj.rb" 
 		required_prj "samples/formatting/prj.rb" 
+		required_prj "samples/class_fields/prj.rb" 
 }
