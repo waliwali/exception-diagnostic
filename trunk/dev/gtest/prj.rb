9cf6db37a5bc1ec3e5445799ece764529e594c65
bsivko <bsivko@3f74e1f5-aec5-da47-6e7e-2e542e26d3da>
2012-09-17 17:23:34 +0000

    +gtest

---
+++ trunk/dev/gtest/prj.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+
+gem 'Mxx_ru', '>= 1.4.7'
+
+require 'mxx_ru/cpp'
+
+MxxRu::Cpp::lib_target {
+
+	include_path( "gtest/1.6.0/include", Mxx_ru::Cpp::Target::OPT_UPSPREAD )
+	include_path( "gtest/1.6.0", Mxx_ru::Cpp::Target::OPT_UPSPREAD )
+
+	implib_path 'lib'
+
+	target 'gtest'
+
+	cpp_source '1.6.0/src/gtest-all.cc'
+}
