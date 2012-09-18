8f0d7073dfb232f805a4981523e162168b38a709
bsivko <bsivko@3f74e1f5-aec5-da47-6e7e-2e542e26d3da>
2012-09-18 09:30:27 +0000

    +sequence test

---
+++ dev/test/sequence/prj.rb
@@ -0,0 +1,18 @@
+
+require 'rubygems'
+
+gem 'Mxx_ru', '>= 1.4.7'
+
+require 'mxx_ru/cpp'
+
+MxxRu::Cpp::exe_target {
+
+	implib_path 'lib'
+
+	target 'sequence.test'
+
+	required_prj "exception_diagnostic/prj.rb" 
+	required_prj 'gtest/prj.rb'
+
+	cpp_source 'sequence.cpp'
+}
