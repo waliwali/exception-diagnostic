920c347ca90859ba66f80bbfcf2c36f4839358a2
bsivko <bsivko@3f74e1f5-aec5-da47-6e7e-2e542e26d3da>
2012-09-18 09:30:27 +0000

    +sequence test

--- trunk/dev/build.rb
+++ trunk/dev/build.rb
@@ -16,6 +16,7 @@ MxxRu::Cpp::composite_target( MxxRu::BUILD_ROOT ) {
 
 		required_prj "gtest/prj.rb" 
 		required_prj "test/base/prj.ut.rb" 
+		required_prj "test/sequence/prj.ut.rb" 
 
 		required_prj "samples/hello_world/prj.rb" 
 		required_prj "samples/several/prj.rb" 
