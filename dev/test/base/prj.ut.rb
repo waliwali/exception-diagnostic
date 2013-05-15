fd0019ed697cb4d3eefbc646c77e65a61ee33038
bsivko <bsivko@3f74e1f5-aec5-da47-6e7e-2e542e26d3da>
2013-05-15 19:32:40 +0000

    +bin win32 vc7 for tag 1.0.2

--- dev/test/base/prj.ut.rb
+++
@@ -1,8 +0,0 @@
-require 'mxx_ru/binary_unittest'
-
-path = 'test/base'
-
-MxxRu::setup_target(
-	MxxRu::BinaryUnittestTarget.new(
-		"#{path}/prj.ut.rb",
-		"#{path}/prj.rb" ) ) 
--- dev/test/sequence/prj.ut.rb
+++
@@ -1,8 +0,0 @@
-require 'mxx_ru/binary_unittest'
-
-path = 'test/sequence'
-
-MxxRu::setup_target(
-	MxxRu::BinaryUnittestTarget.new(
-		"#{path}/prj.ut.rb",
-		"#{path}/prj.rb" ) ) 
